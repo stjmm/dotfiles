f606f15c239e2f4226d7fdf2e0bb154b9ad58bf7
stjmm <shetrynajerkme@gmail.com>
2025-11-29 00:07:14 +0100

    menu

--- scripts/.scripts/menu.sh
+++ scripts/.scripts/menu.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-wmenu-run -l 10 -f "JetBrainsMono Nerd Font 18"
+NORMAL_BG="#15161eD9"
+NORMAL_FG="#c0caf5ff"
+SELECT_BG="#7aa2f7ff"
+SELECT_FG="#15161eff"
+
+FONT="JetBrainsMono Nerd Font 18"
+
+wmenu-run \
+    -i \
+    -l 10 \
+    -N "$NORMAL_BG" \
+    -n "$NORMAL_FG" \
+    -M "$SELECT_BG" \
+    -m "$SELECT_FG" \
+    -S "$SELECT_BG" \
+    -s "$SELECT_FG" \
+    -f "$FONT"
