f82f0c6984454d8c2d4916cdd8b33d500a7b5b91
stjmm <shetrynajerkme@gmail.com>
2026-06-30 09:55:27 +0200

    add xmobar

--- stowables/.config/xmonad/xmonad.hs
+++ stowables/.config/xmonad/xmonad.hs
@@ -1,11 +1,30 @@
 import XMonad
 import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.ManageDocks
+
+myLayoutHook = avoidStruts $ layoutHook def
+
+myXmobarPP :: PP
+myXmobarPP = def
+	{ ppCurrent = xmobarColor "#0db9d7" ""
+	, ppHidden = xmobarColor "#a9b1d6" ""
+	, ppHiddenNoWindows = xmobarColor "#444b6a" ""
+	}
+
+myStatusBar = statusBarProp "xmobar" (pure myXmobarPP)
 
 myKeys =
-	[ ("M-S-<Return>", spawn "alacritty") ]
+	[ ("M-S-<Return>", spawn "alacritty")
+	, ("M-S-r", spawn "xmonad --recompile" >> spawn "xmonad --restart")
+	]
 
-main = xmonad $ def
+main = xmonad $  withEasySB myStatusBar defToggleStrutsKey $ def
 	{ modMask = mod4Mask
 	, terminal = "alacritty"
+	, normalBorderColor = "#2b2b2b"
+	, focusedBorderColor = "#4c566a"
+	, layoutHook = myLayoutHook
 	}
 	`additionalKeysP` myKeys
